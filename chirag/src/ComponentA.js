8462675a421495b516615f6d9c9f853f619e0c1b
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-25 20:12:13 +0530

    function to function

--- chirag/src/ComponentA.js
+++ chirag/src/ComponentA.js
@@ -1,6 +1,7 @@
 import ComponentB from "./ComponentB"
 function ComponentA(){
     let name="prostack"
+    let salary=50000
     let location="marathalli banglore"
     let employee={id:101,name:"chirag",phno:9591}
     let languages=["kannada","english","hindi"]
@@ -8,7 +9,7 @@ function ComponentA(){
       return <div>
         <h1>componentA</h1>
         <hr />
-        <ComponentB name={name} location={location} employee={employee} language={languages}  />
+        <ComponentB name={name} location={location} employee={employee} language={languages} salary={salary} />
       </div>
 }
 export default ComponentA
--- chirag/src/ComponentB.js
+++ chirag/src/ComponentB.js
@@ -3,6 +3,7 @@ function ComponentB(props) {
         <h1>component b</h1><hr />
         <h1>{JSON.stringify(props)}</h1>
         <h2>academy name:{props.name}</h2>
+        <h2>salary:{props.salary}</h2>
         <h2>location:{props.location}</h2>
         <h1>employee_details:{props.employee.id}</h1>
         <h2>known languages:{props.language}</h2>
