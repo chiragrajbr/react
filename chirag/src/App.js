a8eaedff7b9b2b670751730fa6438bce0fcd2751
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-29 10:48:32 +0530

    event handling  in react

--- chirag/src/App.js
+++ chirag/src/App.js
@@ -3,10 +3,12 @@
 //import FunclsA from "./FunclsA"
 //import ClsfunA from "./ClsfunA"
 //import Product from "./state/product"
-import Flip from "./Flip"
+//import Flip from "./Flip"
+import Event1 from "./event/Event1"
+//import Message from "./event/Message"
 function App(){
     return <div>
-       <Flip />
+       <Event1 />
         </div>
 }
 export default App
